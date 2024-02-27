13a0dbabc38aee37c73f3683f802da1d834ca235
Otelia Cronin <67236396+ccsourcecode@users.noreply.github.com>
2024-02-27 12:34:46 +0800

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,7 +5,7 @@
 // You can develop and deploy serverless functions right here as part
 // of your site. Netlify Functions will handle the rest for you.
 
-//const axios = require('axios');
+const axios = require('axios');
 exports.handler = async event => {
     const response = await axios.get('https://ithelp.ithome.com.tw/articles/10315665');
     const subject = event.queryStringParameters.name || 'World'
