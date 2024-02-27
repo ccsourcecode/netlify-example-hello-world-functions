eced6ec92b2fa5b3ab1e1744f793c6f04fa234e4
Otelia Cronin <67236396+ccsourcecode@users.noreply.github.com>
2024-02-27 12:39:25 +0800

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -7,7 +7,7 @@
 
 const axios = require('axios');
 exports.handler = async event => {
-    const response = await axios.get('https://ithelp.ithome.com.tw/articles/10315665');
+    const response = await axios.get('https://www.learnwithjason.dev/blog/serverless-functions/deploy-first-serverless-function/');
     const subject = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
